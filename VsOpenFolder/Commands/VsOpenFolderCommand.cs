0195be649f569769451f2606cf6fdb70d7a4e381
Infinity <Infinity@example.com>
2024-11-09 13:24:24 +0000

    Initial commit

---
+++ VsOpenFolder/Commands/VsOpenFolderCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace VsOpenFolder.Commands;
+
+[Command(PackageIds.OpenFolderCommand)]
+internal sealed class VsOpenFolderCommand : BaseCommand<VsOpenFolderCommand>
+{
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+    {
+        Project project = await VS.Solutions.GetActiveProjectAsync();
+        if (project == null)
+            return;
+
+        string dir = Path.GetDirectoryName(project.FullPath);
+        string buildDir = await project.GetAttributeAsync("OutputPath");
+
+        if (string.IsNullOrEmpty(buildDir) == false)
+            dir = Path.Combine(dir, buildDir);
+
+        Process.Start(dir);
+    }
+}
